a5fb1f9d725423d63efe67081b3e85bd8a2ad620
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-05 16:21:22 +0530

    implementign redux

--- woodo/App.js
+++ woodo/App.js
@@ -1,4 +1,10 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { actionCreators } from './src/reducers/todoListReducer';
+import List from './src/components/reduxDemoList';
+import Input from './src/components/reduxDemoInput';
+import Title from './src/components/reduxDemoTitle';
+
 import { Text, View } from 'react-native';
 import HomePage from './src/components/HomePage';
 import ListViewDemo from './src/components/ListView';
@@ -8,13 +14,42 @@ import InputFieldDemo from './src/components/InputField';
 import ScrollViewDemo2 from './src/components/ScrollView2';
 
 type Props = {};
-export default class App extends Component<Props> {
-  render() {
-    return (    
-		<>
-			<InputFieldDemo />
-		</>
-	
-    );
-  }
+
+const mapStateToProps = (state) => ({
+	todos: state.todos,
+});
+
+class App extends Component<Props> {
+	onAddTodo = (text) => {
+		const { dispatch } = this.props
+
+		dispatch(actionCreators.add(text))
+	}
+
+	onRemoveTodo = (index) => {
+		const { dispatch } = this.props
+
+		dispatch(actionCreators.remove(index))
+	}
+	render() {
+		const { todos } = this.props
+		return (
+			<View>
+				<Title>
+					To-Do List
+        		</Title>
+				<Input
+					placeholder={'Type a todo, then hit enter!'}
+					onSubmitEditing={this.onAddTodo}
+				/>
+				<List
+					list={todos}
+					onPressItem={this.onRemoveTodo}
+				/>
+			</View>
+
+		);
+	}
 }
+
+export default connect(mapStateToProps)(App);
--- woodo/index.js
+++ woodo/index.js
@@ -1,7 +1,17 @@
 /** @format */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import { reducer } from './src/reducers/todoListReducer';
 
-AppRegistry.registerComponent(appName, () => App);
+const store = createStore(reducer);
+const AppWithStore = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => AppWithStore);
