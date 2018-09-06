1d1d635373d48b0665746a66032664cc9fcaa2fa
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-06 16:42:43 +0530

    react-native animation API implemented

--- woodo/App.js
+++ woodo/App.js
@@ -2,19 +2,30 @@ import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 import HomePage from './src/components/HomePage';
 import ListViewDemo from './src/components/ListView';
+import AnimatedApiDemo from './src/components/AnimationApiDemo';
 import ActivityIndicatorDemo from './src/components/ActivityIndicator';
 import FlatListDemo from './src/components/FlatList';
 import InputFieldDemo from './src/components/InputField';
 import ScrollViewDemo2 from './src/components/ScrollView2';
 
+import homePageStyle from './src/styles/homePageStyle';
+import animationDemoStyle from './src/styles/animationDemoStyle';
+
 type Props = {};
 export default class App extends Component<Props> {
   render() {
     return (    
-		<>
-			<InputFieldDemo />
-		</>
-	
+	    <>
+            <View style={homePageStyle.homeContainer} >
+
+                <AnimatedApiDemo style={animationDemoStyle.animationContainer}>
+
+                    <Text style={animationDemoStyle.animatonTextComingSoon}>Coming soon</Text>
+
+                </AnimatedApiDemo>
+
+            </View>
+	    </>
     );
   }
 }
---
+++ woodo/src/styles/animationDemoStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+const animationDemoStyle = StyleSheet.create (
+    {
+        animationContainer: {
+            width: 250, 
+            height: 50, 
+            backgroundColor: 'powderblue'
+        },
+        animatonTextComingSoon: {
+            fontSize: 28, 
+            textAlign: 'center', 
+            margin: 10
+        }
+    }
+);
+
+export default animationDemoStyle;
