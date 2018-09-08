db13ee4b4134946a408808d0272935509113c6bb
Sunil-ghodela <ss.sunil9255@gmail.com>
2018-09-08 15:16:29 +0530

    camera roll getting FACEBOOK undefiend error

--- woodo/index.js
+++ woodo/index.js
@@ -3,13 +3,13 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
-import CameraRollDemo from './src/components/cameraRollDemo';
+import CameraRollDemo from './src/components/CameraRollDemo';
 import { StackNavigator } from 'react-navigation';
 
 
 const Navigation = StackNavigator({
     App: { screen : App },
-    CameraRollDemo: { screen: cameraRollDemo }
+    CameraRollDemo: { screen: CameraRollDemo }
 })
 
 
--- woodo/src/components/StackNavigatorRoute.js
+++ woodo/src/components/StackNavigatorRoute.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import {  Animated, Text, View } from 'react-native';
-import CameraRollDemo from './cameraRollDemo';
+import CameraRollDemo from './CameraRollDemo';
 import { StackNavigator } from 'react-navigation';
 
 type Props = {}
